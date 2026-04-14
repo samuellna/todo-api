31dab0363d1ffe9753a1a63df707a8094b65f3ef
samuellna <sna2@cin.ufpe.br>
2026-04-14 11:10:25 -0300

    feat: change user entity's id type to Long and create user repository with basic methods

--- src/main/java/br/com/samuellna/todo_api/database/model/UserEntity.java
+++ src/main/java/br/com/samuellna/todo_api/database/model/UserEntity.java
@@ -8,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class UserEntity {
-    private int id;
+    private Long id;
     private String name;
     private String email;
 }
--- src/main/java/br/com/samuellna/todo_api/database/repository/UserRepository.java
+++ src/main/java/br/com/samuellna/todo_api/database/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,23 +17,32 @@ public class UserRepository {
         String sqlQuery = "SELECT id, name, email FROM users";
         return jdbcTemplate.query(sqlQuery, (rs, rowNum) ->
             new UserEntity(
-                rs.getInt("id"), rs.getString("name"), rs.getString("email")
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("email")
             )
         );
     }
 
     public Long create(UserEntity user) {
         String sqlQuery = "INSERT INTO users (name, email) VALUES (?, ?) RETURNING id";
-        return jdbcTemplate.queryForObject(sqlQuery, Long.class, user.getName(), user.getEmail());
+        return jdbcTemplate.queryForObject(
+            sqlQuery,
+            Long.class,
+            user.getName(),
+            user.getEmail()
+        );
     }
 
-    public List<UserEntity> findById(String id) {
+    public Optional<UserEntity> findById(Long id) {
         String sqlQuery = "SELECT id, name, email FROM users WHERE id = ?";
-        return jdbcTemplate.query(sqlQuery, (rs, rowNum) ->
-                new UserEntity(
-                        rs.getInt("id"),
-                        rs.getString("name"),
-                        rs.getString("email")
-                ),id);
+        List<UserEntity> user = jdbcTemplate.query(sqlQuery, (rs, rowNum) ->
+            new UserEntity(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("email")
+            ),
+        id);
+        return user.stream().findFirst();
     }
 }
