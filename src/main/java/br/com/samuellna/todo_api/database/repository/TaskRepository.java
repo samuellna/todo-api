d5ab2d6641487b4a57cc63b65dabf14da4fe63da
samuellna <sna2@cin.ufpe.br>
2026-04-17 18:55:44 -0300

    feat: add task repository with crud implemented

---
+++ src/main/java/br/com/samuellna/todo_api/database/repository/TaskRepository.java
@@ -0,0 +1,100 @@
+package br.com.samuellna.todo_api.database.repository;
+
+import br.com.samuellna.todo_api.database.model.TaskEntity;
+import br.com.samuellna.todo_api.dto.TaskDto;
+import br.com.samuellna.todo_api.dto.UpdateTaskDto;
+import br.com.samuellna.todo_api.utils.StatusTask;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class TaskRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<TaskEntity> findAll() {
+        String sqlQuery = "SELECT id, title, description, status, created_at FROM tasks";
+        return jdbcTemplate.query(sqlQuery, (rs, rowNum) ->
+            new TaskEntity(
+                rs.getLong("id"),
+                rs.getString("title"),
+                rs.getString("description"),
+                StatusTask.valueOf(rs.getString("status")),
+                rs.getTimestamp("created_at")
+            )
+        );
+    }
+
+    public Long create(TaskDto task) {
+        String sqlQuery = "INSERT INTO tasks (title, description, status, created_at) VALUES (?, ?, ?, ?) RETURNING id";
+        return jdbcTemplate.queryForObject(
+            sqlQuery,
+            Long.class,
+            task.getTitle(),
+            task.getDescription(),
+            StatusTask.NOT_STARTED.toString(),
+            Timestamp.valueOf(LocalDateTime.now())
+        );
+    }
+
+    public Optional<TaskEntity> findById(Long id) {
+        String sqlQuery = "SELECT id, title, description, status, created_at FROM tasks WHERE id = ?";
+        List<TaskEntity> task = jdbcTemplate.query(sqlQuery,
+            (rs, rowNum) -> new TaskEntity(
+                rs.getLong("id"),
+                rs.getString("title"),
+                rs.getString("description"),
+                StatusTask.valueOf(rs.getString("status")),
+                rs.getTimestamp("created_at")
+            ),
+        id);
+        return task.stream().findFirst();
+    }
+
+    public void update(Long id, UpdateTaskDto taskDto) {
+        List<Object> params = new ArrayList<>();
+        StringBuilder sqlQuery = new StringBuilder("UPDATE tasks SET ");
+
+        if(taskDto.getTitle() != null) {
+            sqlQuery.append("title = ?");
+            params.add(taskDto.getTitle());
+        }
+
+        if(taskDto.getDescription() != null) {
+            sqlQuery.append(", description = ?");
+            params.add(taskDto.getDescription());
+        }
+
+        if(taskDto.getStatus() != null) {
+            sqlQuery.append(", status = ?");
+            params.add(taskDto.getStatus().toString());
+        }
+
+        if(params.isEmpty()) return;
+
+        sqlQuery.append(" WHERE id = ?");
+
+        if(params.size() == 1 &&
+            (params.get(0) == taskDto.getDescription() || params.get(0) == taskDto.getStatus().toString()))
+            sqlQuery.delete(17, 18); // removes the comma
+
+        params.add(id);
+
+        jdbcTemplate.update(
+            sqlQuery.toString(),
+            params.toArray()
+        );
+    }
+
+    public void delete(Long id) {
+        String sqlQuery = "DELETE FROM tasks WHERE id = ?";
+        jdbcTemplate.update(sqlQuery, id);
+    }
+}
